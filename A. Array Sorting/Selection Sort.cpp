e094dd5c0cff0902c8e8c2c4762648d22744f54f
Haffatun Jannat Shifa <125866170+Haffatun@users.noreply.github.com>
2024-07-27 23:04:19 +0600

    Update Selection Sort.cpp

--- A. Array Sorting/Selection Sort.cpp
+++ A. Array Sorting/Selection Sort.cpp
@@ -27,10 +27,11 @@ int main()
 {
     cout << "Koyta integer nite can?\n";
     int n; cin >> n;
-    cout << n << " ta integer input nen\n";
     int arr[n];
+    cout << n << " ta integer input nen\n";
     for(int i=0; i<n; ++i)
         cin >> arr[i];
+    
     selectionSort(arr, n);
     cout << "Array after sorting: ";
     printArray(arr, n);
