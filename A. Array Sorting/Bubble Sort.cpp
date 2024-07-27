2e9224d26a5202e12c8f2bb720b4641cceb44e73
Haffatun Jannat Shifa <125866170+Haffatun@users.noreply.github.com>
2024-07-27 22:47:11 +0600

    Create Bubble Sort.cpp

---
+++ A. Array Sorting/Bubble Sort.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(int ar[], int n)
+{
+    for(int i=0; i<n-1; ++i)
+    {
+        for(int j=0; j<n-1 -i; ++j)
+        {
+            if(ar[j] > ar[j+1])
+                swap(ar[j], ar[j+1]);
+        }
+    }
+}
+
+void printArray(int arr[], int n)
+{
+    for(int i=0; i<n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main()
+{
+    cout << "Koyta integer nite can?\n";
+    int n; cin >> n;
+    cout << n << " ta integer input nen\n";
+    int arr[n];
+    for(int i=0; i<n; ++i)
+        cin >> arr[i];
+    bubbleSort(arr, n);
+    cout << "Array after sorting: ";
+    printArray(arr, n);
+    return 0;
+}
